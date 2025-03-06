d254f76a53c41ab79fb841c9cef2db56717ecb08
Joao Pedro <joaopo.0607@gmail.com>
2025-03-06 00:16:23 -0300

    melhorando meu carregamento, diminuindo a quantidade de commits

--- include/load.py
+++ include/load.py
@@ -6,19 +6,18 @@ from transform import produtos, clientes, endreco_cliente, pedidos, produtos_ped
 Base.metadata.create_all(bind=engine) #estava dando erro pq eu nao tava importando as classes que criei do models, ai nao tinha nenhuma tabela pra ser criada
 
 session = Sessionlocal()
-
-session.bulk_insert_mappings(Produto, produtos)
-session.commit()
-session.bulk_insert_mappings(Cliente, clientes)
-session.commit()
-session.bulk_insert_mappings(Endereco, endreco_cliente)
-session.commit()
-session.bulk_insert_mappings(Pedido, pedidos)
-session.commit()
-session.bulk_insert_mappings(ProdutoPedido, produtos_pedidos)
-session.commit()
-
-session.close()
+try:
+    session.bulk_insert_mappings(Produto, produtos)
+    session.bulk_insert_mappings(Cliente, clientes)
+    session.bulk_insert_mappings(Endereco, endreco_cliente)
+    session.bulk_insert_mappings(Pedido, pedidos)
+    session.bulk_insert_mappings(ProdutoPedido, produtos_pedidos)
+    session.commit()
+except Exception as erro:
+    session.rollback()
+    print(f'Erro ao inserir os dados: {erro}')
+finally:
+    session.close()
 
 
 
