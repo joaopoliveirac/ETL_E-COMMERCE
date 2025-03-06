03bf50810abb97d0012674354ef2970c870c69dd
Joao Pedro <joaopo.0607@gmail.com>
2025-03-06 14:11:51 -0300

    testando minha dag

--- include/load.py
+++ include/load.py
@@ -6,24 +6,27 @@ from transform import produtos, clientes, endreco_cliente, pedidos, produtos_ped
 Base.metadata.create_all(bind=engine) #estava dando erro pq eu nao tava importando as classes que criei do models, ai nao tinha nenhuma tabela pra ser criada
 
 session = Sessionlocal()
-try:
-    def inserir(tabela, dados): #on_conflict_do_nothing() impede a inserção de dados que já existem com a mesma chave primária, como TODAS minhas tabelas tem chave primaria, me permite usar essa abordagem de validação
-        insercao = insert(tabela).values(dados).on_conflict_do_nothing()
-        session.execute(insercao)
-    
-    inserir(Produto,produtos)
-    inserir(Cliente,clientes)
-    inserir(Endereco,endreco_cliente)
-    inserir(Pedido,pedidos)
-    inserir(ProdutoPedido,produtos_pedidos)
+def carregamento(produtos, clientes, enderecos, pedidos, produtos_pedidos):
+    try:
+        def inserir(tabela, dados): #on_conflict_do_nothing() impede a inserção de dados que já existem com a mesma chave primária, como TODAS minhas tabelas tem chave primaria, me permite usar essa abordagem de validação
+            insercao = insert(tabela).values(dados).on_conflict_do_nothing()
+            session.execute(insercao)
+        
+        inserir(Produto,produtos)
+        inserir(Cliente,clientes)
+        inserir(Endereco,endreco_cliente)
+        inserir(Pedido,pedidos)
+        inserir(ProdutoPedido,produtos_pedidos)
 
-    session.commit()
+        session.commit()
 
-except Exception as erro:
-    session.rollback()
-    print(f'Falhou, erro: {erro}')
-finally:
-    session.close()
+    except Exception as erro:
+        session.rollback()
+        print(f'Falhou, erro: {erro}')
+    finally:
+        session.close()
+
+carregar_tudo = carregamento()
 
 
 
