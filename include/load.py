7bb845bc015872384be9803b12412759737a95b6
Joao Pedro <joaopo.0607@gmail.com>
2025-03-05 22:02:07 -0300

    estou com erro de versao no sqlalchemy, por isso minha df.to_sql nao ia.

--- include/load.py
+++ include/load.py
@@ -2,9 +2,15 @@ import pandas as pd
 from db import Base,engine
 from models import Produto, Cliente, Endereco, Pedido, ProdutoPedido
 
-
-Base.metadata.create_all(bind=engine) #aqui ele verifica TODAS as classes que foram registradas como base e cria as tabeças
+Base.metadata.create_all(bind=engine) #
                                         #estava dando erro pq eu nao tava importando as classes que criei do models, ai nao tinha nenhuma tabela pra ser criada
+
+csv_file = 'PRODUTOS.csv'
+df = pd.read_csv(csv_file)
+
+#print(df.head())
+
+df.to_sql("produtos", engine, if_exists="append", index=False)
 
 
 
