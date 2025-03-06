31e6cbd0bd875a103a14d174f8ab9f2b1c050f4d
Joao Pedro <joaopo.0607@gmail.com>
2025-03-05 23:14:27 -0300

    tratamento de cliente, endereco e pedido.

--- include/teste.py
+++ include/teste.py
@@ -1,6 +1,6 @@
 #testar e conhecer os dados da api
 
 import requests
-response = requests.get('https://fakestoreapi.com/products/1')
+response = requests.get('https://fakestoreapi.com/carts/3')
 dados = response.json()
 print(dados)
--- include/transform.py
+++ include/transform.py
@@ -1,10 +1,11 @@
 import pandas as pd
-from extract import dados_brutos_produtos,dados_brutos_clientes,dados_brutos_pedidos
+from datetime import datetime
+from extract import dados_brutos_produtos,dados_brutos_clientes, dados_brutos_pedidos
 
-def transform_produto(dados_brutos):
+def transform_produto(dados_brutos_produtos):
     produtos = []
 
-    for produto in dados_brutos:
+    for produto in dados_brutos_produtos:
         produto_transformado = {'id': produto['id'],
                                 'titulo': produto['title'],
                                 'preco': produto['price'],
@@ -16,5 +17,52 @@ def transform_produto(dados_brutos):
     df = pd.DataFrame(produtos)
     df.to_csv('PRODUTOS.csv', index=False)
 
-transform_produto(dados_brutos)
+def transform_cliente(dados_brutos_clientes):
+    clientes = []
+
+    for cliente in dados_brutos_clientes:
+        clientes = []
+
+        for cliente in dados_brutos_clientes:
+            cliente_transformado = {'id': cliente['id'],
+                                    'nome': cliente['name']['firstname'] + ' ' + cliente['name']['lastname'],
+                                    'email': cliente['email'],
+                                    'telefone': cliente['phone']}
+            
+            clientes.append(cliente_transformado)
+        df = pd.DataFrame(clientes)
+        df.to_csv('CLIENTES.csv', index=False)
+
+def transform_endereco(dados_brutos_clientes):
+    endereco_cliente = []
+
+    for cliente in dados_brutos_clientes:
+        endereco_transformado = {'cliente_id': cliente['id'],
+                                 'cidade': cliente['address']['city'],
+                                 'rua': cliente['address']['street'],
+                                 'numero': cliente['address']['number'],
+                                 'cep': cliente['address']['zipcode']}
+        
+        endereco_cliente.append(endereco_transformado)
+    df = pd.DataFrame(endereco_transformado)
+    df.to_csv('ENDERECO_CLIENTE.csv', index=False)
+
+def transform_pedido(dados_brutos_pedidos):
+    pedidos = []
+
+    for pedido in dados_brutos_pedidos:
+        data = pedido['date']
+        data_obj = datetime.fromisoformat(data.replace('Z', '+00:00'))
+        data_formatada = data_obj.date()
+        pedido_transformado = {'id': pedido['id'],
+                               'cliente_id': pedido['userId'],
+                               'data': data_formatada}
+        
+        pedidos.append(pedido_transformado)
+    df = pd.DataFrame(pedidos)
+    df.to_csv('PEDIDOS.csv', index=False)
+
+
+
+
 
